3d89cf6cb7cde5d304e0cb7f390d7c0cc8b527f4
Samuel Leonildo <samuelleonildo@gmail.com>
2025-09-14 20:41:35 -0300

    style: card style change

--- src/components/BaseContentCard.tsx
+++ src/components/BaseContentCard.tsx
@@ -11,7 +11,7 @@ export interface BaseContentCardProps extends ContentCardProps {
 
 export function BaseContentCard({ solid, children, className }: BaseContentCardProps) {
   return (
-    <div className={clsx('rounded-3xl p-8 md:p-11', { 'bg-solid': solid, 'bg-background-darker': !solid }, className)}>
+    <div className={clsx('rounded-3xl p-8 md:p-11', { 'bg-solid': solid, 'bg-[#f2f2f2] border border-[#DADADA]': !solid }, className)}>
       {children}
     </div>
   );
